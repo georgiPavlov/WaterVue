4d36feedfd661913445ba100a93507eb81fe4fdd
i336317 <georgi.pavlov01@sap.com>
2022-10-08 23:06:14 +0300

    remove unused item in menu

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,8 @@
 import {
   mdiAccountCircle,
   mdiDesktopMac,
-  mdiGithub,
-  mdiLock,
-  mdiAlertCircle,
-  mdiMonitorShimmer,
-  mdiSquareEditOutline,
   mdiTable,
-  mdiViewList,
-  mdiTelevisionGuide,
-  mdiResponsive,
-  mdiPalette
+  mdiResponsive
 } from '@mdi/js'
 
 export default [
@@ -22,13 +14,8 @@ export default [
       label: 'Dashboard'
     }
   ],
-  'Examples',
+  'Menu',
   [
-    {
-      to: '/tables',
-      label: 'Tables',
-      icon: mdiTable
-    },
     {
       to: '/devices',
       label: 'Devices',
@@ -50,67 +37,14 @@ export default [
       icon: mdiTable
     },
     {
-      to: '/forms',
-      label: 'Forms',
-      icon: mdiSquareEditOutline
-    },
-    {
-      to: '/ui',
-      label: 'UI',
-      icon: mdiTelevisionGuide
-    },
-    {
       to: '/responsive',
       label: 'Responsive',
       icon: mdiResponsive
     },
     {
-      to: '/',
-      label: 'Styles',
-      icon: mdiPalette
-    },
-    {
       to: '/profile',
       label: 'Profile',
       icon: mdiAccountCircle
-    },
-    {
-      to: '/login',
-      label: 'Login',
-      icon: mdiLock
-    },
-    {
-      to: '/error',
-      label: 'Error',
-      icon: mdiAlertCircle
-    },
-    {
-      label: 'Submenus',
-      subLabel: 'Submenus Example',
-      icon: mdiViewList,
-      menu: [
-        {
-          label: 'Sub-item One'
-        },
-        {
-          label: 'Sub-item Two'
-        }
-      ]
-    }
-  ],
-  'About',
-  [
-    {
-      href: 'https://justboil.me/tailwind-admin-templates/vue-dashboard/',
-      label: 'Premium version',
-      icon: mdiMonitorShimmer,
-      target: '_blank'
-    },
-    {
-      href: 'https://github.com/justboil/admin-one-vue-tailwind',
-      label: 'GitHub',
-      icon: mdiGithub,
-      target: '_blank'
     }
   ]
 ]
