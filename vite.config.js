711b2e3d4070d34dcf53eb447aaa809554cfa110
i336317 <georgi.pavlov01@sap.com>
2022-10-11 23:05:39 +0300

    vite 3.x

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true
   },
-  plugins: [vue(), mkcert()],
+  plugins: [
+    vue(),
+    mkcert(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        workbox: {
+          maximumFileSizeToCacheInBytes: 50000000
+        }
+      }
+    )
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': resolve(__dirname, 'src'),
+      vue: 'vue/dist/vue.esm-bundler.js'
     }
-  }
+  },
+  base: './'
 })
