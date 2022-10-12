b509523156f80483e228a6bdd062e67bd8d443c6
i336317 <georgi.pavlov01@sap.com>
2022-10-13 00:36:05 +0300

    change main request port

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ export default new Vuex.Store({
   state: {
     tokenType: 'Bearer ',
     token: '',
-    baseURL: 'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com',
+    baseURL: 'http://water-me-lb-842691727.eu-central-1.elb.amazonaws.com:8000',
     options: {
       headers: {
         Authorization: 'none'
